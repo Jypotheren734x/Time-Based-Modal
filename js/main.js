5099464b1d780bbc569555639ea73e2eca9a2354
Nickolas Komarnitsky <komarnickolas@gmail.com>
2017-06-01 16:16:02 -0600

    Update Styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 /**
  * Created by komar on 5/30/2017.
  */
+setTimeout(function () {
+    var modal = document.getElementsByClassName('modal');
+    for(i = 0; i<modal.length; i++){
+        modal[i].classList.add('show');
+    }
+}, 3000);
+document.getElementById('close').addEventListener("click", function () {
+    var modal = document.getElementsByClassName('modal');
+    for(i = 0; i<modal.length; i++){
+        modal[i].classList.remove('show');
+    }
+});
+document.getElementById('submit').addEventListener("click", function () {
+   document.getElementById('modal').innerHTML = "<h1 class='modal-title'>THANKS!</h1><a href='#' class='close' id='close'>&times;</a>";
+});
